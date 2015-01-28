e753e690321d57fa780b67a02de094731319b44a
maria <maria@ephimera.org>
2015-01-28 12:56:18 +0100

    updated sha in variant_decorator

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -8,7 +8,7 @@ module Spree
 
     private
 
-    durably_decorate :create_stock_items, mode: 'soft', sha: '40a37fdea4f3d8a1fe774911a603c013862545f5' do
+    durably_decorate :create_stock_items, mode: 'soft', sha: '98704433ac5c66ba46e02699f3cf03d13d4f1281' do
       StockLocation.all.each do |stock_location|
         if stock_location.supplier_id.blank? || self.suppliers.pluck(:id).include?(stock_location.supplier_id)
           stock_location.propagate_variant(self) if stock_location.propagate_all_variants?
